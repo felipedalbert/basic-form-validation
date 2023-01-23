e3eb807a722e9c05882e85b090eddfd05e905265
Felipe Dalbert <felipeoficial195@gmail.com>
2023-01-23 18:25:50 -0300

    javascript clean code update

--- js/validation.js
+++ js/validation.js
@@ -1,48 +1,51 @@
 const inputLine = document.querySelectorAll('.input-line')
 const sendButton = document.querySelector('button')
 
-
-sendButton.addEventListener('click', () =>{
-
-    inputLine.forEach(thisInputLine =>{
-
-        const inputData = thisInputLine.querySelectorAll('[placeholder]')
-        const errorAlert = thisInputLine.querySelector('span')
-
-        inputData.forEach(item =>{
-
-            if(item.value == ''){
-                thisInputLine.classList.add('error-validation')
-                item.classList.add('error-validation')
-                errorAlert.classList.add('error-validation')
-
-            }
-        })
-    })
-})
-
 inputLine.forEach(thisInputLine => {
-    const inputData = thisInputLine.querySelectorAll('[placeholder]')
+    const inputData = thisInputLine.querySelector('[placeholder]')
     const errorAlert = thisInputLine.querySelector('span')
 
-    inputData.forEach(item => {
-        item.addEventListener('change', () =>{
-            if(item.value !== ''){
-                thisInputLine.classList.remove('error-validation')
-                item.classList.add('correct-validation')
-                errorAlert.classList.remove('error-validation')
+    function errorValidation(){
+        inputData.classList.add('error-validation')
+        thisInputLine.classList.add('error-validation')
+        errorAlert.classList.add('error-validation')
+    }
 
-            }else if(item.value == ''){
-                item.classList.remove('correct-validation')
-                item.classList.add('error-validation')
-                thisInputLine.classList.add('error-validation')
-                errorAlert.classList.add('error-validation')
+    function correctValidation(){
+        thisInputLine.classList.remove('error-validation')
+        inputData.classList.remove('error-validation')
+        errorAlert.classList.remove('error-validation')
+        inputData.classList.add('correct-validation')
+    }
 
-                if(item.classList.contains('error-validation')){
-                    thisInputLine.classList.add('error-validation')
-                    errorAlert.classList.add('error-validation')
+    sendButton.addEventListener('click', () =>{
+
+        if(inputData.value == ''){
+            errorValidation()
+
+            inputData.addEventListener('change', () =>{
+                if(inputData.value == ''){
+                    errorValidation()
                 }
-            }
-        })
+            })
+
+        }else if(inputData.value !== ''){
+            inputData.addEventListener('change', () =>{
+                if(inputData.value == ''){
+                    errorValidation()
+                }
+            })
+        }
+    
+    })
+
+    inputData.addEventListener('change', () =>{
+        if(inputData.value !== ''){
+            correctValidation()
+
+        }else if(inputData.value == ''){
+            inputData.classList.remove('correct-validation')
+        }
     }) 
 })
+
