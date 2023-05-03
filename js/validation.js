8188bf392f26ea7bfdd5ccc8408cf17d71e223c5
Felipe Dalbert <felipeoficial195@gmail.com>
2023-05-03 20:26:53 -0300

    perf(forEach): evitando 4 submits

--- js/validation.js
+++ js/validation.js
@@ -16,16 +16,22 @@ function correctValidation(thisInputLine, inputData){
     inputData.classList.add('correct-validation')
 }
 
-inputLine.forEach(thisInputLine => {
-    const inputData = thisInputLine.querySelector('[placeholder]')
 
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault()
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    inputLine.forEach(thisInputLine => {
+        const inputData = thisInputLine.querySelector('[placeholder]')
 
         function insideValidation(){
             inputData.addEventListener('input', () =>{
                 if(inputData.value == ''){
                     errorValidation(thisInputLine, inputData)
+                    inputData.classList.remove('correct-validation')
+                }else if(inputData.value !== ''){
+                    correctValidation(thisInputLine, inputData)
+                    
                 }
             })
         }
@@ -35,30 +41,24 @@ inputLine.forEach(thisInputLine => {
 
             insideValidation()
         }else if(inputData.value !== ''){
+            correctValidation(thisInputLine, inputData)
+
             insideValidation()
         }
 
-        if(deleteCounter && deleteCounter.length === 4){
-            orientationText.innerHTML = 'Mensagem eviada com sucesso!'
-
-            setTimeout(1000)
-
-            form.submit()
-        }
-    })
-
-    inputData.addEventListener('input', () =>{
-        if(inputData.value !== ''){
-            correctValidation(thisInputLine, inputData)
-            console.log('caiu embaixo')
-
-        }else if(inputData.value == ''){
-            inputData.classList.remove('correct-validation')
-        }
-    })
-
-    inputData.addEventListener('change', ()=>{
         deleteCounter = document.querySelectorAll('.correct-validation')
     })
+    
+    if(deleteCounter && deleteCounter.length === 4){
+        orientationText.innerHTML = 'Mensagem eviada com sucesso!'
+
+        setTimeout(1000)
+
+        form.submit()
+    }
+
+    orientationText.style.color = 'green'
+    
 })
+
 
